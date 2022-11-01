53c4b6c692c78298c5f339782459297c4c2d25b2
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-01 15:21:36 -0500

    test 1 for home route + test 2 for route with :id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiacs } = require('../lib/zodiac-data');
 
 describe('example test - you should probably update me', () => {
   it('home route should return hello world', async () => {
@@ -9,4 +10,26 @@ describe('example test - you should probably update me', () => {
 });
 
 // test 1 is expected to show list of all zodiac names and id on /zodiac route
-// test 2 is expected to show details of the zodiac based on the id
+
+describe('zodiac routes', () => {
+  test('/zodiac route should display list of zodiac names with id', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiacs.map((zodiac) => {
+      return { id: zodiac.id, name: zodiac.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  // test 2 is expected to show details of the zodiac based on the id
+  test('/zodiac/:id route should display the zodiac + details with matching id', async () => {
+    const res = await request(app).get('/zodiac/1');
+    const expected = {
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    };
+    expect(res.body).toEqual(expected);
+  });
+  // test 3 is expected to show the horoscope route from an object we need to make
+});
